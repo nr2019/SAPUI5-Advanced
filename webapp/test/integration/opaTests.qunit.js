13bf8e20d1ecc8479e2fd8382f6e243398b15fe1
nahuel.ryzy <nahuel.ryzy@gmail.com>
2023-07-01 23:24:50 +0000

    Primer commit a 03_Proyecto_Final

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["nryzy/employees/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nryzy/employees/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
