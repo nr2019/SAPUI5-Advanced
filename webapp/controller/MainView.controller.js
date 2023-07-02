ce6b6de67c0dcac0b3e510866a9e060b11a4e9f6
nahuel.ryzy <nahuel.ryzy@gmail.com>
2023-07-02 01:07:14 +0000

    Primer commit en nueva rama 04

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -7,9 +7,33 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("nryzy.employees.controller.MainView", {
-            onInit: function () {
+        /*function myCheck(){
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
 
+            if (valueEmployee.length === 6){
+                inputEmployee.setDescription("OK");
+            } else{
+                inputEmployee.setDescription("Not OK");
             }
-        });
+        }*/
+
+        var Main = Controller.extend("nryzy.employees.controller.MainView", {});
+            // Cuando se dispara el onValidate, llama al myCheck
+            //onValidate: myCheck
+        Main.prototype.onValidate = function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                    // inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+
+                } else {
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
+            }
+        return Main;
     });
