c2bfc881881f245678c48010dec1de7fb45fec1c
nahuel.ryzy <nahuel.ryzy@gmail.com>
2023-07-03 00:02:07 +0000

    Error al leer archivo

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,39 +1,48 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
-        /*function myCheck(){
+        function onInit() {
+
+            let oView = this.getView(),
+                i18nBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle(),
+                oJSONModel = new JSONModel();
+            
+            oJSONModel.loadData("../localService/mockdata/Employees.json");
+            oJSONModel.attachParseError(function(oEvent) { console.log(oEvent); });
+            // Obtengo la vista
+            
+            //se vincula el modelo a la vista
+            oView.setModel(oJSONModel);
+        }
+
+
+        var Main = Controller.extend("nryzy.employees.controller.MainView", {});
+        // Cuando se dispara el onValidate, llama al myCheck
+        //onValidate: myCheck
+        Main.prototype.onValidate = function () {
             var inputEmployee = this.byId("inputEmployee");
             var valueEmployee = inputEmployee.getValue();
 
-            if (valueEmployee.length === 6){
-                inputEmployee.setDescription("OK");
-            } else{
-                inputEmployee.setDescription("Not OK");
+            if (valueEmployee.length === 6) {
+                // inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+
+            } else {
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
             }
-        }*/
+        };
 
-        var Main = Controller.extend("nryzy.employees.controller.MainView", {});
-            // Cuando se dispara el onValidate, llama al myCheck
-            //onValidate: myCheck
-        Main.prototype.onValidate = function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
+        Main.prototype.onInit = onInit;
 
-                if (valueEmployee.length === 6) {
-                    // inputEmployee.setDescription("OK");
-                    this.getView().byId("labelCountry").setVisible(true);
-                    this.getView().byId("slCountry").setVisible(true);
-
-                } else {
-                    this.getView().byId("labelCountry").setVisible(false);
-                    this.getView().byId("slCountry").setVisible(false);
-                }
-            }
         return Main;
     });
