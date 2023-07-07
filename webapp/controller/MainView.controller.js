fc88d69102f5805ddef81b70ab5248afad8c25e5
nahuel.ryzy <nahuel.ryzy@gmail.com>
2023-07-07 10:12:58 +0000

    07-07-23 -Modelos y eventos completo

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         function onInit() {
@@ -23,6 +27,34 @@ sap.ui.define([
             oView.setModel(oJSONModel);
         }
 
+        function onFilter(){
+            var oJSON = this.getView().getModel().getData();
+            var filters = [];
+            // Sobre el filters armo los filtros que quiero que tenga la tabla
+            if (oJSON.EmployeeId !== ""){
+                // EmployeeID es un campo del modelo (archivo json que cargo)
+                // EmployeeId es el ID que le asigné al inputEmployee en la vista
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
+            }
+
+            if (oJSON.CountryKey !== ""){
+                // Country es un campo del modelo (archivo json que cargo)
+                // CountryKey es el ID que le asigné al slCountry en la vista
+                filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+
+            // Luego de armar los filtros que quiero aplicar, se actualiza la tabla de 
+            // la vista con los filtros armados
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+        }
+
+        function onClearFilter(){
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+        }
 
         var Main = Controller.extend("nryzy.employees.controller.MainView", {});
         // Cuando se dispara el onValidate, llama al myCheck
@@ -43,6 +75,8 @@ sap.ui.define([
         };
 
         Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter;
 
         return Main;
     });
